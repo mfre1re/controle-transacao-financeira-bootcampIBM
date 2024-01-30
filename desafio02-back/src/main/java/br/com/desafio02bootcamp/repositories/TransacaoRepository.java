1063b2ec8b81a4838b2e2d04cf6a8ba9e5d8d79d
Miguel Freire <88170132+mfre1re@users.noreply.github.com>
2024-01-30 18:02:58 -0300

    Update TransacaoRepository.java

--- desafio02-back/src/main/java/br/com/desafio02bootcamp/repositories/TransacaoRepository.java
+++ desafio02-back/src/main/java/br/com/desafio02bootcamp/repositories/TransacaoRepository.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 @Repository
 public interface TransacaoRepository extends JpaRepository<TransacaoEntity, Integer> {
-    //List<TransacaoEntity> findByCategoriaNome(String categoria);
     List<TransacaoEntity> findByCategoriaId(Integer categoriaId);
 
 }
